362903278ccf6fbd427ed3803fe4f36bfcc09690
saqfromchurchvillage <saku.riihijarvi@gmail.com>
2024-06-18 21:14:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,15 +98,22 @@ st.pyplot(fig)
 st.subheader("Arvostelujen jakauma")
 rating_counts = st.session_state.reviews["Rating"].value_counts().sort_index()
 fig, ax = plt.subplots()
-ax.bar(rating_counts.index, rating_counts.values, color='lightgreen', width=0.4)
+bars = ax.bar(rating_counts.index, rating_counts.values, color='lightgreen', width=0.4)
 ax.set_xticks([i for i in range(6)])  # Näytä luvut 0-5 x-akselilla
 ax.set_xlim(-0.5, 5.5)  # Aseta x-akselin rajoitukset
-ax.set_ylim(0, max(rating_counts.values) + 1)  # Aseta y-akselin rajoitukset
+ax.set_ylim(0, max(rating_counts.values, default=1) + 1)  # Aseta y-akselin rajoitukset
 ax.set_xlabel("Rating")
 ax.set_ylabel("Arvostelujen lukumäärä")
 ax.set_title("Arvostelujen jakauma")
 ax.yaxis.get_major_locator().set_params(integer=True)  # Näytä vain kokonaisluvut y-akselilla
+
+# Aseta pylväiden väli
+for bar in bars:
+    bar.set_width(0.8)  # Leventää pylväitä
+    bar.set_x(bar.get_x() - 0.2)  # Siirtää pylväitä vasemmalle, jotta ne eivät mene päällekkäin
+
 st.pyplot(fig)
+
 
 
 # Heatmap of ratings by reviewer and beer
