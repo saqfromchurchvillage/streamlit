ce09400c11e832b5b90ae4a22e2bf380ced8a306
saqfromchurchvillage <saku.riihijarvi@gmail.com>
2024-06-18 21:06:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,10 +74,7 @@ st.markdown(st.session_state.reviews.to_html(classes='wide-table'), unsafe_allow
 # Top 5 beers
 st.subheader("Top 5 Oluet")
 top_beers = st.session_state.reviews.groupby(["Oluen nimi", "Tyyppi"]).agg(Keskiarvo=("Rating", "mean"), Arvosteluja=("Rating", "count")).reset_index()
-top_beers = top_beers.sort_values(by="Keskiarvo", ascending=False).head(5)
-
-# Luo uusi DataFrame indeksoinnilla 0-4
-#top_beers.reset_index(drop=True, inplace=True)
+top_beers = top_beers.sort_values(by="Keskiarvo", ascending=True).head(5)
 
 # Muuta DataFrame HTML:ksi ilman indeksiä
 top_beers_html = top_beers.to_html(classes='wide-table', index=False)
