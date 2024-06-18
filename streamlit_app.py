0d8518496c83c8e327fce42cfece6c4215bbd247
saqfromchurchvillage <saku.riihijarvi@gmail.com>
2024-06-18 20:59:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,8 @@ st.markdown(st.session_state.reviews.to_html(classes='wide-table'), unsafe_allow
 st.subheader("Top 5 Oluet")
 top_beers = st.session_state.reviews.groupby(["Oluen nimi", "Tyyppi"]).agg(Keskiarvo=("Rating", "mean"), Arvosteluja=("Rating", "count")).reset_index()
 top_beers = top_beers.sort_values(by="Keskiarvo", ascending=False).head(5)
-top_beers.index = range(1, len(top_beers) + 1)  # Aloita indeksointi numerosta 1
+top_beers.reset_index(drop=True, inplace=True)
+top_beers.index += 1  # Aloita indeksointi numerosta 1
 st.markdown(top_beers.to_html(classes='wide-table'), unsafe_allow_html=True)
 
 # Average ratings per beer
